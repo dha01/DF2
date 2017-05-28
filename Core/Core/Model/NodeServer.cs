7b27ab7efe444be22761d5025159383b17499721
dha <dha01@yandex.ru>
2017-05-28 22:53:47 +0500

    Добавлен метод для получение данных по сетевому адресу узла

--- Core/Core/Model/NodeServer.cs
+++ Core/Core/Model/NodeServer.cs
@@ -11,47 +11,52 @@ using Newtonsoft.Json;
 
 namespace Core.Model
 {
-    public class NodeServer
-    {
-	    private INodeRepository _nodeRepository;
-	    private IServerService _serverService;
+	public class NodeServer
+	{
+		private INodeRepository _nodeRepository;
+		private IServerService _serverService;
 
 
 
 		public NodeServer(IServerService server_service, INodeRepository node_repository)
-	    {
-		    _nodeRepository = node_repository;
-		    _serverService = server_service;
-			
-		    Task.Factory.StartNew(() =>
-		    {
-			    while (true)
-			    {
-				    Thread.Sleep(1000);
+		{
+			_nodeRepository = node_repository;
+			_serverService = server_service;
+			/*
+			Task.Factory.StartNew(() =>
+			{
+				while (true)
+				{
+					Thread.Sleep(1000);
 
 					var result =  node_repository.Ping(new Node() { NetworkAddress = new NetworkAddress() { URI = "http://192.168.1.4", Port = 5000} });
 					Console.WriteLine(result);
 				}
-		    });
+			});*/
 
-	    }
+		}
 
-	    public bool Ping()
-	    {
+		public NetworkAddress GetNetworkAddress()
+		{
+			return _serverService.NetworkAddress;
+		}
+
+		public bool Ping()
+		{
 			Console.WriteLine("Pinged");
 			return true;
-	    }
+		}
 
-	    public void Run()
-	    {
-		    Task.Factory.StartNew(() =>
-		    {
-			    _serverService.Run();
-		    });
-	    }
-	    public void Stop()
-	    {
-		    _serverService.Stop();
-	    }
+		public void Run()
+		{
+			Task.Factory.StartNew(() =>
+			{
+				_serverService.Run();
+			});
+		}
+		public void Stop()
+		{
+			_serverService.Stop();
+		}
 	}
 }
--- Node/Node/Controllers/NodeController.cs
+++ Node/Node/Controllers/NodeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Model;
+using Core.Model.Network.Node.DataModel;
 using Core.Model.Network.Node.Repository;
 using Core.Model.Network.WebMethod.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -10,21 +11,26 @@ using Node.Static;
 
 namespace Node.Controllers
 {
-    public class NodeController : Controller
-    {
+	public class NodeController : Controller
+	{
 		public NodeController()
-	    {
+		{
+		}
+
+		public NetworkAddress GetNetworkAddress()
+		{
+			return StaticVariables.NodeServer.GetNetworkAddress();
 		}
 
 		public bool Ping()
-        {
-            return StaticVariables.NodeServer.Ping();
-        }
+		{
+			return StaticVariables.NodeServer.Ping();
+		}
 
 		[HttpPost]
 		public string GetValue([FromBody]Person person)
-        {
-            return "value!";
+		{
+			return "value!";
 		}
 	}
 }
