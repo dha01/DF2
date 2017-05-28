81852a05bb3a5c2f8ef005556e489cf7f9a9fabe
dha <dha01@yandex.ru>
2017-05-28 20:52:19 +0500

    Read вместо ReadKey

--- Node/Node/Program.cs
+++ Node/Node/Program.cs
@@ -77,12 +77,12 @@ namespace Node
 	}
 
 	class Program
-    {
-	    static async Task MainAsync()
-	    {
-		    using (var client = new HttpClient())
-		    {
-			    client.BaseAddress = new Uri("http://localhost:4000");
+	{
+		static async Task MainAsync()
+		{
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("http://localhost:4000");
 
 				/*   JsonConvert.SerializeObject(new {id = 125});
 				   var content = new FormUrlEncodedContent(new KeyValuePair<string, string>[]
@@ -90,114 +90,114 @@ namespace Node
 					   new KeyValuePair<string, string>("id", "125")
 				   });*/
 				var json = JsonConvert.SerializeObject(new Person
-			    {
-				    Age = 10,
+				{
+					Age = 10,
 					FirstName = "a",
 					LastName = "b"
-			    });
+				});
 				var data = new StringContent(content: json,
-				    encoding: Encoding.UTF8,
-				    mediaType: "application/json");
+					encoding: Encoding.UTF8,
+					mediaType: "application/json");
 
 
 				var result = await client.PostAsync("/Values/GetValue", data);
-			    string resultContent = await result.Content.ReadAsStringAsync();
-			    Console.WriteLine(resultContent);
-		    }
-	    }
+				string resultContent = await result.Content.ReadAsStringAsync();
+				Console.WriteLine(resultContent);
+			}
+		}
 
-	    public static string GetLocalIPAddress()
-	    {
-		    var host = Dns.GetHostEntryAsync(Dns.GetHostName()).Result;
-		    foreach (var ip in host.AddressList)
-		    {
-			    if (ip.AddressFamily == AddressFamily.InterNetwork)
-			    {
-				    return ip.ToString();
-			    }
-		    }
-		    throw new Exception("Local IP Address Not Found!");
-	    }
+		public static string GetLocalIPAddress()
+		{
+			var host = Dns.GetHostEntryAsync(Dns.GetHostName()).Result;
+			foreach (var ip in host.AddressList)
+			{
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+				{
+					return ip.ToString();
+				}
+			}
+			throw new Exception("Local IP Address Not Found!");
+		}
 
 		static void Main(string[] args)
-        {
+		{
 			try
-	        {
+			{
 				/*
 
-		        Task.Factory.StartNew(() =>
-		        {
-			        while (true)
-			        {
-				        Thread.Sleep(5000);
-				        MainAsync();
-			        }
-		        });*/
+				Task.Factory.StartNew(() =>
+				{
+					while (true)
+					{
+						Thread.Sleep(5000);
+						MainAsync();
+					}
+				});*/
 
-		        var newtwork_address = new NetworkAddress() { URI = "http://localhost", IPv4 = IPAddress.Any, Port = 5000 };
-
-
-		        StaticVariables.NodeServer = new NodeServer(new ServerService(newtwork_address), new NodeRepository(new WebMethodRepository()));
-
-		        /*var host = new WebHostBuilder()
-			        .UseKestrel()
-			        .UseContentRoot(Directory.GetCurrentDirectory())
-			        .UseIISIntegration()
-			        .UseStartup<Startup>()
-			        .UseUrls("http://localhost:4000")
-			        .Build();
+				var newtwork_address = new NetworkAddress() { URI = "http://localhost", IPv4 = IPAddress.Any, Port = 5000 };
 
 
-		        host.Run();*/
-		        StaticVariables.NodeServer.Run();
-		        Console.WriteLine("Running demo with Kestrel.");
-		        Console.ReadKey();
-		        StaticVariables.NodeServer.Stop();
-		        Console.WriteLine("Stop demo with Kestrel.");
-		        Console.ReadKey();
-		        StaticVariables.NodeServer.Run();
-		        Console.WriteLine("Running demo with Kestrel.");
-		        Console.ReadKey();
-		        StaticVariables.NodeServer.Stop();
-		        Console.WriteLine("Stop demo with Kestrel.");
-		        Console.ReadKey();
+				StaticVariables.NodeServer = new NodeServer(new ServerService(newtwork_address), new NodeRepository(new WebMethodRepository()));
+
+				/*var host = new WebHostBuilder()
+					.UseKestrel()
+					.UseContentRoot(Directory.GetCurrentDirectory())
+					.UseIISIntegration()
+					.UseStartup<Startup>()
+					.UseUrls("http://localhost:4000")
+					.Build();
+
+
+				host.Run();*/
+				StaticVariables.NodeServer.Run();
+				Console.WriteLine("Running demo with Kestrel.");
+				Console.Read();
+				StaticVariables.NodeServer.Stop();
+				Console.WriteLine("Stop demo with Kestrel.");
+				Console.Read();
+				StaticVariables.NodeServer.Run();
+				Console.WriteLine("Running demo with Kestrel.");
+				Console.Read();
+				StaticVariables.NodeServer.Stop();
+				Console.WriteLine("Stop demo with Kestrel.");
+				Console.Read();
 
 
 
-		        /*
-		        var config = new ConfigurationBuilder()
-		           // .AddCommandLine(args)
-			        .Build();
+				/*
+				var config = new ConfigurationBuilder()
+				   // .AddCommandLine(args)
+					.Build();
 
-		        var builder = new WebHostBuilder()
-			        .UseContentRoot(Directory.GetCurrentDirectory())
-			        .UseConfiguration(config)
-			        .UseStartup<StartupWithGreeting>()
-			        .ConfigureServices(service =>
-			        {
-				        service.AddSingleton<IGreeting, MorningGreeting>();
-			        })
-			        .UseKestrel(options =>
-			        {
-				        if (config["threadCount"] != null)
-				        {
-					        options.ThreadCount = int.Parse(config["threadCount"]);
-				        }
-			        })
-			        .UseUrls("http://localhost:5000");
+				var builder = new WebHostBuilder()
+					.UseContentRoot(Directory.GetCurrentDirectory())
+					.UseConfiguration(config)
+					.UseStartup<StartupWithGreeting>()
+					.ConfigureServices(service =>
+					{
+						service.AddSingleton<IGreeting, MorningGreeting>();
+					})
+					.UseKestrel(options =>
+					{
+						if (config["threadCount"] != null)
+						{
+							options.ThreadCount = int.Parse(config["threadCount"]);
+						}
+					})
+					.UseUrls("http://localhost:5000");
 
-		        var host = builder.Build();
-		        host.Run();*/
+				var host = builder.Build();
+				host.Run();*/
 
-		        //   return 0;
+				//   return 0;
 			}
 			catch (Exception e)
-	        {
-		        Console.WriteLine(e);
-	        }
+			{
+				Console.WriteLine(e);
+			}
 
-	        Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 		
 		private static object WebHostBuilder()
 		{
