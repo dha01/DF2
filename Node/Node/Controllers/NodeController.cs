ca0c41e54866e900922539a2bb4a033f17c01733
dha <dha01@yandex.ru>
2017-07-03 01:38:19 +0500

    Добавлена возможность использования Razor

--- Node/Node/Controllers/NodeController.cs
+++ Node/Node/Controllers/NodeController.cs
@@ -1,6 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Microsoft.Extensions.Options;
 using Node.Static;
+using RazorLight;
+using RazorLight.Extensions;
 
 namespace Node.Controllers
 {
@@ -10,9 +17,20 @@ namespace Node.Controllers
 		{
 		}
 
-		public Core.Model.OpenInterfaces.Node.DataModel.Node GetInfo()
+		public ActionResult GetInfo()
 		{
-			return StaticVariables.NodeServer.GetInfo();
+			/*string templatePath = $@"{Directory.GetCurrentDirectory()}\Views\Node";
+			IRazorLightEngine engine = EngineFactory.CreatePhysical(templatePath);
+
+			// Strings and anonymous models
+			string stringResult = engine.ParseString("Hello @Model.Name", new { Name = "John" });
+
+			// Files and strong models
+			string resultFromFile = engine.Parse("GetInfo.cshtml", StaticVariables.NodeServer.GetInfo());
+			
+			return resultFromFile;*/
+			
+			return View(StaticVariables.NodeServer.GetInfo());
 		}
 
 		public Core.Model.OpenInterfaces.Node.DataModel.Node AddNode(string address)
@@ -34,6 +52,11 @@ namespace Node.Controllers
 		public string GetValue([FromBody]Person person)
 		{
 			return "value!";
+		}
+
+		public ActionResult A()
+		{
+			return View("Index");
 		}
 	}
 }
